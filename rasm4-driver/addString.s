b0018128269d4cbf9223f10e28df2d7a476ffc48
james <00johnsonjp@gmail.com>
2023-04-17 12:22:46 -0700

    driver update

--- rasm4-driver/addString.s
+++ rasm4-driver/addString.s
@@ -11,7 +11,9 @@ ptr:         .quad 0
 szEnterFile: .asciz "Enter file name: "
 szFileName:  .skip 64
 szFile:      .asciz "input.txt"
+szEOL:       .asciz "end of line"
 fileBuf:     .skip 512
+fileBufCpy:  .skip 512
 iFD:         .byte 0
     .global addString
     .text
@@ -23,6 +25,8 @@ addString:
     b.eq fromFile   // goto fromFile
     ret lr          // if for some reason the input is incorrect it will return
 
+    fromText:
+            ret lr
     fromFile:
             str lr, [sp, -16]
             stp x1, x2, [sp, -16]
@@ -35,17 +39,20 @@ addString:
             ldr x0,=szFileName  // loading filename str
             mov x1, #64          // 64 bytes max input
             bl getstring        // getting input
-            ldr x0,=szFileName
+            ldr x1,=szFileName
             // ------
 
             // open the textfile for reading
             mov x0, #AT_FDCWD
             mov x8, #56
-            ldr x1,=szFile
+            //ldr x1,=szFile
             mov x2, #R
             mov x3, #RW_RW____
             svc 0
-            
+            // ------
+
+            cmp w0, #0
+            b.le exit
             ldr x4,=iFD
             strb w0,[x4]
             
@@ -58,23 +65,26 @@ addString:
                 
                 ldr x0,=fileBuf
                 bl putstring
+                ldr x0,=ptr
+                bl ll
 
                 ldr x0,=iFD
-                ldr w0,[x0]
-
+                ldrb w0,[x0]
+                
                 b top1
             last:
                 ldr x0,=iFD
                 ldrb w0,[x0]
                 mov x8, #57
                 svc 0
+                //b printNodes
             exit:
                 ldp x1, x2, [sp, -16]
                 ldr lr, [sp], 16
                 ret
             getchar:
-                //ldr x0,=iFD
-                //ldr x0,[x0]
+                ldr x0,=iFD
+                ldrb w0,[x0]
                 mov x2, #1
                 mov x8, #63
                 svc 0
@@ -85,7 +95,6 @@ addString:
                 bl getchar
                 ldrb w2, [x1]
                 cmp w2, #0xa
-
                 b.eq EOLINE
 
                 cmp w0, #0x0
@@ -103,6 +112,8 @@ addString:
                 add x1, x1, #1
                 mov w2, #0
                 strb w2,[x1]
+                //ldr x0,=szEOL
+                //bl putstring
                 b skip
             EOF:
                 mov x19, x0
@@ -115,5 +126,3 @@ addString:
             skip:
                 ldr x30, [sp], #16
                 ret
-    fromText:
-            ret lr
--- rasm4-driver/ll.s
+++ rasm4-driver/ll.s
@@ -1,5 +1,7 @@
-// LINKED LIST FUNCTIONS
-// ASSUMES HEAD IS IN X0, AND TAIL IN X1 FOR ALL OPERATIONS
+// PARAMS @
+// X0: HEAD, IF NULL THEN WILL CREATE NODE
+// X1: DATA
+
     .global ll
     .data
 node_size:
@@ -13,6 +15,9 @@ ll:
         stp x3, x4, [sp, -16]!      // copy of x3 and x4 
         stp x19, x20, [sp, -16]!    // copy of x19 and x20        
         stp x21, x1, [sp, -16]!      // copy of head and data
+        ldr x0,[x0]
+        cmp x0, #0
+        b.ne addBack
         mov x20, x0     // storing data in x20
         mov x0, node_size
         bl malloc
@@ -23,31 +28,16 @@ ll:
         ldr lr, [sp], 16
         ret
 
-    addFront:
-        // node in x0
-        // head in x1
-        str lr, [sp, -16]!          // copy of lr
-        str x1, [sp, -16]!
-        
-        ldr x1, [x1]
-        str x1, [x0, #8]
-
-        ldr x1, [sp], 16
-        ldr lr, [sp], 16
-
-        ret
-    addBack:
-        // node in x0
-        // tail in x1
-        str lr, [sp, -16]!          // copy of lr
-        stp x3, x4, [sp, -16]!      // copy of x3 and x4 
-        stp x19, x20, [sp, -16]!    // copy of x19 and x20
-        stp x0, x1, [sp, -16]!      // copy of tail and data
-
-
-        ldp x0, x1, [sp], 16
-        ldp x19, x20, [sp], 16
-        ldp x3, x4, [sp], 16
-        ldr lr, [sp], 16
-        ret
+addBack:
+        mov x19, x0
+        mov x21, #0
+        .addLoop:
+            ldr x20, [x19, #8]
+            cmp x20, #0
+            b.eq .foundLast
+            add x21, x21, #1
+            mov x19, x20
+            ldr x19, [x19]
+            b .addLoop
+        .foundLast:
 
